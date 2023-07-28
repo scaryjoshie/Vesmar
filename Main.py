f1b670f4067a76096019740955f58c1db8d567fa
Divkov575 <dmitriy@ivkov.net>
2023-07-28 14:29:18 -0400

    updated for 7 col format & some changes to output dir

--- Main.py
+++ Main.py
@@ -12,7 +12,6 @@ from multiprocessing import Pool
 import pandas as pd
 import openpyxl
 from openpyxl.styles import PatternFill
-from openpyxl.utils.dataframe import dataframe_to_rows
 
 from src.TableOperations import Table
 from src.MiscUtils import location_to_cell_id, foo
@@ -24,26 +23,20 @@ config_parser.read("config.ini")
 config_parser.sections()
 config = config_parser["config"]
 
-INPUT_DIR = config["INPUT_DIR"]
-OUTPUT_DIR = config["OUTPUT_DIR"]
-OVERRIDE_EXISTING_OUTPUT = config["OVERRIDE_EXISTING_OUTPUT"]
-QUOTIENT_MAX = int(config["QUOTIENT_MAX"])
-EXCLUDE_FILES = json.loads(config["EXCLUDE_FILES"])  # not yet implemented
-COLOR_DICT = dict(config_parser.items("COLOR_DICT"))
-
 # color mapping
 fillers = {}
-for color in COLOR_DICT.keys():
-    temp = PatternFill(patternType="solid", fgColor=COLOR_DICT[color])
+# a = config
+color_dict = dict(config_parser.items("COLOR_DICT"))
+for color in color_dict.keys():
+    temp = PatternFill(patternType="solid", fgColor=color_dict[color])
     fillers[color] = temp
 
 # create/overwrite output dir
-full_output_path = os.path.join("Output", OUTPUT_DIR)
-if OVERRIDE_EXISTING_OUTPUT and os.path.exists(full_output_path):
-    shutil.rmtree(full_output_path)
-if not os.path.exists("Output"):
-    os.mkdir("Output")
-os.mkdir(full_output_path)
+if config['OVERRIDE_EXISTING_OUTPUT'] and os.path.exists(config['OUTPUT_DIR']):
+    shutil.rmtree(config['OUTPUT_DIR'])
+else:
+    AssertionError("Please Clear or Change Output Dir")
+os.mkdir(config['OUTPUT_DIR'])
 
 
 def process_spreadsheet(file_path):
@@ -56,13 +49,12 @@ def process_spreadsheet(file_path):
     # Creates table object and checks all values
     table = Table(df=df)
     table.check_dates()
-    table.check_normals(QUOTIENT_MAX=QUOTIENT_MAX)
+    table.check_normals(QUOTIENT_MAX=int(config['QUOTIENT_MAX']))
     table.check_labels()
 
     # Loads file into openpyxl
     wb = openpyxl.load_workbook(file_path)
     ws = wb["Table_0"]
-    # rows = dataframe_to_rows(table)
 
     # Fills every cell value with what it's supposed to be
     for cell in table.df_list:
@@ -75,45 +67,8 @@ def process_spreadsheet(file_path):
 
     # Names and saves file
     name = os.path.basename(file_path)
-    wb.save(os.path.join("Output", OUTPUT_DIR, name))
-
-
-def optimize(save_optimziation=True):
-    """
-    Calculates the optimal number of cores by finding the optimum of the function here: https://www.desmos.com/calculator/xcfduba49k
-    Finds the fastest time based on process creation overhead and execution time
-    d/d(#processes) (overhead * #processes + (number_input_data / #processes) * function_execution_time)
-
-    The optimization is negligible, I just wasted a bunch of time on it and feel bad removing it.
-    Feel free to remove it. Im just too attached to do it myself.
-    This function is pretty excessive since results should be consistent across all systems.
-    """
-
-    OUTPUT_DIR = "tmp"
-    FILES = glob.glob(f"{INPUT_DIR}/*.xlsx")
-
-    # time process execution time
-    start_1 = time.perf_counter()
-    process_spreadsheet(FILES[0])
-    time_1 = time.perf_counter() - start_1
-
-    # timing process creation overhead
-    with Pool() as pool:
-        start_2 = time.perf_counter()
-        pool.map(foo, [0])
-        time_2 = time.perf_counter() - start_2
-
-    # find optimal number of cores to complete the process
-    optimal_concurrency_count = float(pow(len(FILES) * time_1 / time_2, 0.5))
-    print(f"exact optimal process count: {optimal_concurrency_count}")
-    if save_optimziation:
-        config_parser["config"]["PROCESSES"] = str(round(optimal_concurrency_count))
-        with open("config.ini", "w") as configfile:
-            config_parser.write(configfile)
-        print(f"PROCESSES COUNT set to: {round(optimal_concurrency_count)}")
-    else:
-        print("PROCESSES COUNT not updated")
-
+    wb.save(os.path.join(config['OUTPUT_DIR'], name))
+    
 
 # Process all files in parallel
 if __name__ == "__main__":
@@ -121,7 +76,7 @@ if __name__ == "__main__":
 
     with Pool(processes=4) as pool:  # concurrent execution
         pool.map(
-            process_spreadsheet, glob.glob(f"{INPUT_DIR}/*.xlsx")
+            process_spreadsheet, glob.glob(f"{config['INPUT_DIR']}/*.xlsx")
         )  # inputs are a list of files in the input_dir -> passed to func
 
     # completion message + total time
